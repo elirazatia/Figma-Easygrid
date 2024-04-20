bacddc635fb520f92ee2163440b1967e8a31efea
Eliraz Atia <elirazatia003@gmail.com>
2024-04-20 08:52:44 +0100

    Added swipe gestures to the combine cells tool

--- scripts/components/Grid.js
+++ scripts/components/Grid.js
@@ -8,13 +8,13 @@ function Grid(selector) {
     el.appendChild(table)
 
     // Generate the visual for swiping / merging cells - \\\ NOT WORKING YET ///
-    const swipeGesture = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    el.appendChild(swipeGesture)
-    swipeGesture.style.position = 'absolute'
-    swipeGesture.style.pointerEvents = 'none'
+    const canvas = document.createElement('canvas')
+    const swipeGesture = canvas.getContext('2d')
+    el.appendChild(canvas)
+    canvas.style.position = 'absolute'
+    canvas.style.pointerEvents = 'none'
 
     // Store the cells
-    let cells = []
     let cellConnections = []
 
     let rows = []
@@ -60,7 +60,7 @@ function Grid(selector) {
             cellConnections
         )
 
-        
+
         // Insert all the bounds
         bounds.bounds.forEach((boundContainer, boundIndex) => boundContainer.forEach(subchild => {
             const td = renderCell(
@@ -79,6 +79,11 @@ function Grid(selector) {
         // Get the size of the table
         table.style.width = `${bounds.xFor(Math.inf)}px`
         table.style.height = `${bounds.yFor(Math.inf)}px`
+
+        canvas.style.width = `${bounds.xFor(Math.inf)}px`
+        canvas.style.height = `${bounds.yFor(Math.inf)}px`
+        canvas.setAttribute('width', bounds.xFor(Math.inf))
+        canvas.setAttribute('height', bounds.yFor(Math.inf))
     }
 
 
@@ -92,28 +97,58 @@ function Grid(selector) {
         __hoverEffectEl.forEach(el => el.classList.add('__hover'))
     }
 
+    const mouseEventCoordinateToTable = (mouseEvent) => {
+        let theElement = mouseEvent.target
+        let x = mouseEvent.layerX
+        let y = mouseEvent.layerY
+
+        return [
+            x + parseFloat(theElement.style.left.replace('px', '')),
+            y + parseFloat(theElement.style.top.replace('px', ''))
+        ]
+    }
+
     const toolEvents = {
         draw: {
-            init(initCell) { return [initCell.x, initCell.y] },
-            move(cell) {
+            init(initCell, mouseEvent) {
+                swipeGesture.strokeStyle = "red";
+                swipeGesture.moveTo(...mouseEventCoordinateToTable(mouseEvent))
+
+                return [initCell.x, initCell.y]
+            },
+            move(cell, mouseEvent) {
+                swipeGesture.lineTo(...mouseEventCoordinateToTable(mouseEvent))
+                swipeGesture.stroke()
+
                 const isAlreadySelected = mergeCellSession.filter(existingCell => 
                     existingCell.x === cell.x && 
                     existingCell.y === cell.y).length === 1
                 if (!isAlreadySelected) mergeCellSession.push(cell)
             },
-            release() { return { type: 'draw', cells: mergeCellSession.map(cell => [cell.x, cell.y]) } }
+            release(mouseEvent) { return { type: 'draw', cells: mergeCellSession.map(cell => [cell.x, cell.y]) } }
         },
         merge: {
-            init(initCell) { return {
+            init(initCell, mouseEvent) { return {
+                initMousePosition: mouseEventCoordinateToTable(mouseEvent),
                 initCell: [initCell.x, initCell.y],
                 offsetX: 1,
                 offsetY: 1
             } },
-            move(cell) {
+            move(cell, mouseEvent) {
+                const mousePosition = mouseEventCoordinateToTable(mouseEvent)
+                swipeGesture.reset()
+                swipeGesture.strokeStyle = "red";
+                swipeGesture.setLineDash([5,2])
+                swipeGesture.strokeRect(
+                    mousePosition[0],
+                    mousePosition[1],
+                    mergeCellSession.initMousePosition[0] - mousePosition[0],
+                    mergeCellSession.initMousePosition[1] - mousePosition[1]);
+
                 mergeCellSession.offsetX = cell.x - mergeCellSession.initCell[0]
                 mergeCellSession.offsetY = cell.y - mergeCellSession.initCell[1]
             },
-            release() {
+            release(mouseEvent) {
                 if (mergeCellSession.offsetX === 0 && mergeCellSession.offsetY === 0)
                     return
 
@@ -138,32 +173,40 @@ function Grid(selector) {
         if (e.target?.cell == null) return
 
         // Generate initial merge session
-        mergeCellSession = toolEvents[mergeCellTool].init(e.target.cell)
+        mergeCellSession = toolEvents[mergeCellTool].init(e.target.cell, e)
+        setHoverEffectEl([])
     })
 
     table.addEventListener('mouseleave', (e) => {
         // Remove hover effect
         setHoverEffectEl([])
+        
+        // Clear effects
+        swipeGesture.reset()
+        mergeCellSession = null
     })
 
     table.addEventListener('mousemove', (e) => {
-        // Hover effect
-        const hoverIndex = e.target?.getAttribute('bound-index')
-        setHoverEffectEl((hoverIndex !== null)
-            ? table.querySelectorAll(`[bound-index='${hoverIndex}']`)
-            : [])
+        if (!mergeCellSession) {
+            // Only show hover effect if user is not dragging
+            // Hover effect
+            const hoverIndex = e.target?.getAttribute('bound-index')
+            setHoverEffectEl((hoverIndex !== null)
+                ? table.querySelectorAll(`[bound-index='${hoverIndex}']`)
+                : [])
 
-        // Return if user is not pressing down
-        if (!mergeCellSession) return
+            // Return if user is not pressing down
+            return
+        }
         if (e.target?.cell == null) return
 
         // Perform tool function
-        toolEvents[mergeCellTool].move(e.target.cell)
+        toolEvents[mergeCellTool].move(e.target.cell, e)
     })
 
     table.addEventListener('mouseup', (e) => {
         // Push the connections
-        const newConnectionModel = toolEvents[mergeCellTool].release()
+        const newConnectionModel = toolEvents[mergeCellTool].release(e)
         cellConnections.push(newConnectionModel)
         cellConnections = cellConnections.filter(i => (i)) // Filter out the new model if it returns new - i.e. Invalid
 
